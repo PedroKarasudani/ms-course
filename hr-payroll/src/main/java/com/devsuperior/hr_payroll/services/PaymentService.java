f439c35d634fec16e50ec9eeb91e9b447025b7c6
Pedro Karasudani <pedro.karasudani@hotmail.com>
2025-01-27 21:41:11 -0300

    Implementar projeto hr-payroll (mock)

---
+++ hr-payroll/src/main/java/com/devsuperior/hr_payroll/services/PaymentService.java
@@ -0,0 +1,12 @@
+package com.devsuperior.hr_payroll.services;
+
+import com.devsuperior.hr_payroll.entities.Payment;
+import org.springframework.stereotype.Service;
+
+@Service
+public class PaymentService {
+
+    public Payment getPayment(long workerId, int days) {
+        return new Payment("Bob",200.0 ,days);
+    }
+}
